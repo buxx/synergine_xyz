e825b64dad85801879b502d0046aa93f3a72b9f7
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-06-05 15:09:45 +0200

    rebuild

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import synergine_xyz
 
 setup(
     name='synergine_xyz',
-    version='0.0.1.8',
+    version='0.0.1.9',
     packages=find_packages(),
     install_requires=['synergine'],
     author='Bastien Sevajol',
